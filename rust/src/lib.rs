32c6554f77565af809083d2eb3a3241497ae17f0
Charlie Harrington <charles.harrington@gmail.com>
2022-11-01 08:34:03 -0700

    6 tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,32 @@
 use primitive_types::U256;
 
-pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
-    // TODO: Implement me
+const STOP: u8 = 0;
+const ADD: u8 = 1;
+const POP: u8 = 80;
+const PUSH1: u8 = 96;
 
-    return Vec::new();
+pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {    
+    let mut v = Vec::new();
+    let c = code.as_ref();
+    println!("{:?}", c);
+    let mut i = 0;
+    while i < c.len() {
+        if c[i] == PUSH1 {
+            i += 1;
+            v.insert(0, U256::from(c[i]));   
+        }
+        if c[i] == POP {
+            v.remove(0);
+        }
+        if c[i] == STOP {
+            break;
+        }
+        if c[i] == ADD {
+            let a = v.pop().unwrap_or_else(|| U256::from(0));
+            let b = v.pop().unwrap_or_else(|| U256::from(0));
+            v.insert(0, a+b);
+        }
+        i += 1;
+    }
+    return v;
 }
