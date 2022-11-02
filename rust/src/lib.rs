f54c300655591905824a1b8e8aaf82fdae3e82ca
Charlie Harrington <charles.harrington@gmail.com>
2022-11-01 18:42:37 -0700

    9 tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,31 +2,65 @@ use primitive_types::U256;
 
 const STOP: u8 = 0;
 const ADD: u8 = 1;
+const MUL: u8 = 2;
 const POP: u8 = 80;
 const PUSH1: u8 = 96;
+const PUSH32: u8 = 127;
 
 pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {    
-    let mut v = Vec::new();
+    // convert instructions
     let c = code.as_ref();
     println!("{:?}", c);
-    let mut i = 0;
-    while i < c.len() {
-        if c[i] == PUSH1 {
-            i += 1;
-            v.insert(0, U256::from(c[i]));   
+    
+    // iti result
+    let mut v = Vec::new();
+    
+    // init program counter
+    let mut pc = 0;
+    
+    // process instructions
+    while pc < c.len() {
+        let instruction = c[pc];
+        match instruction {
+            PUSH1 => {
+                pc += 1;
+                // inserts at index 0
+                v.insert(0, U256::from(c[pc]));
+            },
+            PUSH32 => {
+                pc += 1;
+                let slice = &c[pc..pc+32];
+                println!("slice is {:?}: ", slice);
+                let number = U256::from_little_endian(slice);
+                println!("number is {:?}: ", number);
+                v.insert(0, number);
+            }
+            POP => {
+                // removes index 0
+                v.remove(0);
+            },
+            STOP => {
+                break;
+            }
+            ADD => {
+                let a = v.pop().unwrap_or_else(|| U256::from(0));
+                let b = v.pop().unwrap_or_else(|| U256::from(0));
+                let (sum, _) = U256::overflowing_add(a, b);
+                v.insert(0, sum);
+            }
+            MUL => {
+                let a = v.pop().unwrap_or_else(|| U256::from(0));
+                let b = v.pop().unwrap_or_else(|| U256::from(0));
+                let (product, _) = U256::overflowing_mul(a, b);
+                v.insert(0, product);
+            }
+            _ => {
+                println!("unsupported instruction!");
+            }
         }
-        if c[i] == POP {
-            v.remove(0);
-        }
-        if c[i] == STOP {
-            break;
-        }
-        if c[i] == ADD {
-            let a = v.pop().unwrap_or_else(|| U256::from(0));
-            let b = v.pop().unwrap_or_else(|| U256::from(0));
-            v.insert(0, a+b);
-        }
-        i += 1;
+        
+        // advance program counter
+        pc += 1;
     }
     return v;
 }
