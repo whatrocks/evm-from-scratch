b9b93a0fc10c802b29fa71618f9f1d60d129a72a
Charlie Harrington <charles.harrington@gmail.com>
2022-11-05 12:06:46 -0700

    all push ops

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -24,6 +24,34 @@ const POP: u8 = 80;
 const PUSH1: u8 = 96;
 const PUSH2: u8 = 97;
 const PUSH3: u8 = 98;
+const PUSH4: u8 = 99;
+const PUSH5: u8 = 100;
+const PUSH6: u8 = 101;
+const PUSH7: u8 = 102;
+const PUSH8: u8 = 103;
+const PUSH9: u8 = 104;
+const PUSH10: u8 = 105;
+const PUSH11: u8 = 106;
+const PUSH12: u8 = 107;
+const PUSH13: u8 = 108;
+const PUSH14: u8 = 109;
+const PUSH15: u8 = 110;
+const PUSH16: u8 = 111;
+const PUSH17: u8 = 112;
+const PUSH18: u8 = 113;
+const PUSH19: u8 = 114;
+const PUSH20: u8 = 115;
+const PUSH21: u8 = 116;
+const PUSH22: u8 = 117;
+const PUSH23: u8 = 118;
+const PUSH24: u8 = 119;
+const PUSH25: u8 = 120;
+const PUSH26: u8 = 121;
+const PUSH27: u8 = 122;
+const PUSH28: u8 = 123;
+const PUSH29: u8 = 124;
+const PUSH30: u8 = 125;
+const PUSH31: u8 = 126;
 const PUSH32: u8 = 127;
 
 pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
@@ -47,27 +75,97 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
                 stack.insert(0, U256::from(c[pc]));
             }
             PUSH2 => {
-                pc += 1;
-                let slice = &c[pc..pc + 2];
-                let number = U256::from_big_endian(slice);
-                stack.insert(0, number);
-                pc += 1;
+                pc += push_n_bytes(2, &mut stack, &c[pc + 1..pc + 3]);
             }
             PUSH3 => {
-                pc += 1;
-                let slice = &c[pc..pc + 3];
-                let number = U256::from_big_endian(slice);
-                stack.insert(0, number);
-                pc += 2;
+                pc += push_n_bytes(3, &mut stack, &c[pc + 1..pc + 4]);
+            }
+            PUSH4 => {
+                pc += push_n_bytes(4, &mut stack, &c[pc + 1..pc + 5]);
+            }
+            PUSH5 => {
+                pc += push_n_bytes(5, &mut stack, &c[pc + 1..pc + 6]);
+            }
+            PUSH6 => {
+                pc += push_n_bytes(6, &mut stack, &c[pc + 1..pc + 7]);
+            }
+            PUSH7 => {
+                pc += push_n_bytes(7, &mut stack, &c[pc + 1..pc + 8]);
+            }
+            PUSH8 => {
+                pc += push_n_bytes(8, &mut stack, &c[pc + 1..pc + 9]);
+            }
+            PUSH9 => {
+                pc += push_n_bytes(9, &mut stack, &c[pc + 1..pc + 10]);
+            }
+            PUSH10 => {
+                pc += push_n_bytes(10, &mut stack, &c[pc + 1..pc + 11]);
+            }
+            PUSH11 => {
+                pc += push_n_bytes(11, &mut stack, &c[pc + 1..pc + 12]);
+            }
+            PUSH12 => {
+                pc += push_n_bytes(12, &mut stack, &c[pc + 1..pc + 13]);
+            }
+            PUSH13 => {
+                pc += push_n_bytes(13, &mut stack, &c[pc + 1..pc + 14]);
+            }
+            PUSH14 => {
+                pc += push_n_bytes(14, &mut stack, &c[pc + 1..pc + 15]);
+            }
+            PUSH15 => {
+                pc += push_n_bytes(15, &mut stack, &c[pc + 1..pc + 16]);
+            }
+            PUSH16 => {
+                pc += push_n_bytes(16, &mut stack, &c[pc + 1..pc + 17]);
+            }
+            PUSH17 => {
+                pc += push_n_bytes(17, &mut stack, &c[pc + 1..pc + 18]);
+            }
+            PUSH18 => {
+                pc += push_n_bytes(18, &mut stack, &c[pc + 1..pc + 19]);
+            }
+            PUSH19 => {
+                pc += push_n_bytes(19, &mut stack, &c[pc + 1..pc + 20]);
+            }
+            PUSH20 => {
+                pc += push_n_bytes(20, &mut stack, &c[pc + 1..pc + 21]);
+            }
+            PUSH21 => {
+                pc += push_n_bytes(21, &mut stack, &c[pc + 1..pc + 22]);
+            }
+            PUSH22 => {
+                pc += push_n_bytes(22, &mut stack, &c[pc + 1..pc + 23]);
+            }
+            PUSH23 => {
+                pc += push_n_bytes(23, &mut stack, &c[pc + 1..pc + 24]);
+            }
+            PUSH24 => {
+                pc += push_n_bytes(24, &mut stack, &c[pc + 1..pc + 25]);
+            }
+            PUSH25 => {
+                pc += push_n_bytes(25, &mut stack, &c[pc + 1..pc + 26]);
+            }
+            PUSH26 => {
+                pc += push_n_bytes(26, &mut stack, &c[pc + 1..pc + 27]);
+            }
+            PUSH27 => {
+                pc += push_n_bytes(27, &mut stack, &c[pc + 1..pc + 28]);
+            }
+            PUSH28 => {
+                pc += push_n_bytes(28, &mut stack, &c[pc + 1..pc + 29]);
+            }
+            PUSH29 => {
+                pc += push_n_bytes(29, &mut stack, &c[pc + 1..pc + 30]);
+            }
+            PUSH30 => {
+                pc += push_n_bytes(30, &mut stack, &c[pc + 1..pc + 31]);
+            }
+            PUSH31 => {
+                pc += push_n_bytes(31, &mut stack, &c[pc + 1..pc + 32]);
             }
             PUSH32 => {
-                pc += 1;
-                let slice = &c[pc..pc + 32];
-                // println!("slice is {:?}: ", slice);
-                let number = U256::from_big_endian(slice);
-                // println!("number is {:?}: ", number);
-                stack.insert(0, number);
-                pc += 31
+                pc += push_n_bytes(32, &mut stack, &c[pc + 1..pc + 33]);
             }
             POP => {
                 stack.remove(0);
@@ -286,4 +384,10 @@ pub fn evm(code: impl AsRef<[u8]>) -> Vec<U256> {
         pc += 1;
     }
     return stack;
+}
+
+fn push_n_bytes(n: usize, stack: &mut Vec<U256>, bytes: &[u8]) -> usize {
+    let number = U256::from_big_endian(bytes);
+    stack.insert(0, number);
+    return n;
 }
